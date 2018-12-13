3e2433ef863dff32f7ea62fd12f4df9a8c736acf
yancymin <senseslofter@gmail.com>
2018-12-13 17:23:49 +0800

    added footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -62,7 +62,34 @@
           </div>
         </section>
       </div>
+
+      <footer>
+        <div class="footer-wrap">
+          <div class="copyright-info">
+            <img
+              src="../assets/footer_logo.svg"
+              alt=""
+            >
+            <p>
+              Copyright © 2018 EGO
+              <br>
+              <br>
+              武汉极意网络科技有限公司<br>
+              鄂 ICP 备 12016193 号 -1<br>
+              增值电信业务经营许可证鄂 B2-20170058
+            </p>
+          </div>
+          <div class="info">
+            <ul>
+              <li><a href="">服务条款</a></li>
+              <li><a href="">隐私声明</a></li>
+              <li>合作请发送邮件至 <a href="mailto:contact@egoid.me">contact@egoid.me</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
     </div>
+
   </div>
 </template>
 
@@ -173,7 +200,7 @@ export default {
         @include fontMedium;
 
         &:before {
-          content:'';
+          content: "";
           display: inline-block;
           width: 24px;
           height: 3px;
@@ -225,18 +252,73 @@ export default {
       }
     }
   }
-}
+  footer {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color: $dark;
 
-p {
-  @include fontNormal;
+    .footer-wrap {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      width: 1180px;
+      padding: 40px 0 24px 0;
 
-  &:first-child {
-    @include h1(46px, 46px);
-    @include fontBold;
-  }
-  &:last-child {
-    @include h2(36px, 36px);
-    @include fontLight;
+      .copyright-info {
+        img {
+          margin-bottom: 32px;
+        }
+        p {
+          font-family: source-han-sans-simplified-c, sans-serif;
+          font-weight: 300;
+          font-style: normal;
+          width: 218px;
+          font-size: 12px;
+          line-height: 19px;
+          color: $textColor-2;
+          opacity: 0.8;
+        }
+      }
+    }
+
+    .info {
+      width: 390px;
+      ul {
+        display: flex;
+        justify-content: space-between;
+
+        li {
+          margin-left: 22px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          @include p(12px, 12px);
+
+          &:last-child {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            a {
+              margin-left: 8px;
+            }
+          }
+
+          a {
+            text-decoration-line: none;
+            transition: $normalTs;
+            @include p(13px, 13px);
+            
+
+            &:hover {
+              color: $borderColor;
+            text-decoration-line: underline;
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
